cfb8d86e90ae8a1ae3463b896a83009bbc2d4f0e
Mirza Dzinic <mirzadzinic@Mirzas-MacBook-Pro.local>
2025-10-27 19:34:17 +0100

    Deleting lock button and adding rotation function (function is buggy and is not working at the moment)

--- Assets/MyGame/Scripts/RotationScript.cs
+++ Assets/MyGame/Scripts/RotationScript.cs
@@ -1,16 +1,42 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class RotationScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public TMP_Text text_button_left;
+    public TMP_Text text_button_right;
+
+    public GameObject sphere;
+
+    public float rotation_speed = 10;
     void Start()
     {
-        
+        text_button_left.text = "Rotate left";
+        text_button_right.text = "Rotate right";
     }
 
-    // Update is called once per frame
+
+
+    public void rotateLeft()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            sphere.transform.Rotate(Vector3.left * rotation_speed * Time.deltaTime);
+        }
+    }
+
+    public void rotateRight()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            sphere.transform.Rotate(Vector3.right * rotation_speed * Time.deltaTime);
+        }
+    }
+
+
     void Update()
     {
-        
+
     }
 }
