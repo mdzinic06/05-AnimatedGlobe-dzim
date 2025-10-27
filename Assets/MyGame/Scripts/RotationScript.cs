bd23601606bbe51f563921dd3de4315afadcb51a
Mirza Dzinic <mirzadzinic@Mirzas-MacBook-Pro.local>
2025-10-27 20:12:19 +0100

    rotating globe and starting button but stopping button is not working as expected

--- Assets/MyGame/Scripts/RotationScript.cs
+++ Assets/MyGame/Scripts/RotationScript.cs
@@ -1,42 +1,64 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class RotationScript : MonoBehaviour
 {
     public TMP_Text text_button_left;
     public TMP_Text text_button_right;
 
-    public GameObject sphere;
+    public TMP_Text text_stop_rotation_left;
 
-    public float rotation_speed = 10;
+    public TMP_Text text_stop_rotation_right;
+
+    public GameObject sphere;
+    public float rotationSpeed = 100f;
+
+    private bool rotateLeftActive = false;
+    private bool rotateRightActive = false;
+
     void Start()
     {
-        text_button_left.text = "Rotate left";
-        text_button_right.text = "Rotate right";
+        text_button_left.text = "Rotate Left";
+        text_button_right.text = "Rotate Right";
+        text_stop_rotation_left.text = "Stop rotation left";
+        text_stop_rotation_right.text = "Stop rotation right";
     }
 
-
-
-    public void rotateLeft()
+    public void StartRotateLeft()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse2))
-        {
-            sphere.transform.Rotate(Vector3.left * rotation_speed * Time.deltaTime);
-        }
+        rotateLeftActive = true;
     }
 
-    public void rotateRight()
+    public void StopRotateLeft()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse2))
-        {
-            sphere.transform.Rotate(Vector3.right * rotation_speed * Time.deltaTime);
-        }
+        rotateLeftActive = false;
     }
 
+    public void StartRotateRight()
+    {
+        rotateRightActive = true;
+    }
+
+    public void StopRotateRight()
+    {
+        rotateRightActive = false;
+    }
 
     void Update()
     {
+        if (!rotateLeftActive && !rotateRightActive) return;
 
+        float rotation = rotationSpeed * Time.deltaTime;
+
+        if (rotateLeftActive)
+        {
+            sphere.transform.Rotate(Vector3.up, rotation, Space.World);
+        }
+        else if (rotateRightActive)
+        {
+            sphere.transform.Rotate(Vector3.up, -rotation, Space.World);
+        }
     }
 }
